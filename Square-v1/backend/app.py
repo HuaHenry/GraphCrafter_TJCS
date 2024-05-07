066d9f7e1d470392c520c53f8a5ef541e98d1609
YtShenn <2306143594@qq.com>
2024-05-07 05:30:05 +0000

    Replace app.py

--- Square-v1/backend/app.py
+++ Square-v1/backend/app.py
@@ -1,9 +1,85 @@
 from flask import Flask, jsonify,render_template
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+import sys,os
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__, static_url_path='/', static_folder='./../frontend/dist', template_folder='./../frontend/dist')
- 
-DEBUG = True
+# 创建数据库
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+
+class User(db.Model):  # 用户
+    id = db.Column(db.Integer, primary_key=True)  # 主键，账号
+    name = db.Column(db.String(20))  # 名字
+    password=db.Column(db.String(20))  # 密码
+    photo = db.Column(db.String(60))  # 头像
+    email = db.Column(db.String(254))  # 邮箱
+    age = db.Column(db.Integer)  # 年龄
+    sex = db.Column(db.Boolean)  # 性别，1是男0是女
+    senior = db.Column(db.Boolean)  # 是否为付费用户
+    
+class Post(db.Model):  # 帖子
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    author_id = db.Column(db.Integer)      # 作者id
+    date = db.Column(db.DateTime)     # 日期
+    picture1 = db.Column(db.String(60))     # 图片1，默认为封面
+    picture2 = db.Column(db.String(60))     # 图片2
+    picture3 = db.Column(db.String(60))     # 图片3
+    picture4 = db.Column(db.String(60))     # 图片4
+    picture5 = db.Column(db.String(60))     # 图片5
+    title = db.Column(db.String(60))     # 标题
+    body = db.Column(db.Text)  # 正文
+    model = db.Column(db.Integer)  # 模型参数id
+
+class Comment(db.Model):  # 评论
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    date = db.Column(db.DateTime)     # 日期
+    content = db.Column(db.Text)  # 正文
+    author_id = db.Column(db.Integer)      # 作者id
+    post_id = db.Column(db.Integer)      # 文章id
+
+class Like(db.Model):  # 点赞
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    date = db.Column(db.DateTime)     # 日期
+    author_id = db.Column(db.Integer)      # 作者id
+    post_id = db.Column(db.Integer)      # 文章id
+
+class Collect( db.Model):  # 收藏
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    date = db.Column(db.DateTime)     # 日期
+    user_id = db.Column(db.Integer)      # 作者id
+    post_id = db.Column(db.Integer)      # 文章id
+
+class Follow(db.Model):  # 关注
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    follower_id = db.Column(db.Integer)      # 粉丝id
+    followed_id = db.Column(db.Integer)      # 被关注者id
+    status = db.Column(db.Integer)      # 关注状态,0表示单独关注，1表示互相关注
+
+class FeedBack(db.Model):  # 用户反馈
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    date = db.Column(db.DateTime)     # 日期
+    user_id = db.Column(db.Integer)      # 用户id
+    content = db.Column(db.Text)  # 正文
+    processed = db.Column(db.Boolean)  # 是否已处理
+    back_content = db.Column(db.Text)  # 反馈内容
+
+class ModifyHistory(db.Model):  # 修图历史
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    date = db.Column(db.DateTime)     # 日期
+    user_id = db.Column(db.Integer)      # 用户id
+    pic_before = db.Column(db.String(60))     # 修改前图片
+    pic_after = db.Column(db.String(60))     # 修改后图片
+    prompt = db.Column(db.Text)  # 提示
+    model_id = db.Column(db.Integer)  # 模型参数
  
 # app = Flask(__name__)
 app.config.from_object(__name__)
