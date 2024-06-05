3a891b8b41319a011832507364d29606c46d180f
depth <2306143594@qq.com>
2024-06-06 02:34:08 +0800

    添加对话修图界面存草稿，及加载动画

--- frontend/src/pages/p2p.vue
+++ frontend/src/pages/p2p.vue
@@ -1,40 +1,69 @@
 <template>
-  <div class="custom-box">
+  <div class="custom-box" >
+    
     <!--顶栏-->
     <div class="header">
       <div class="room-info">对话修图模型</div>
     </div>
     <!--对话框-->
-    <div ref="content" class="message-container">
+    <div ref="content" class="message-container" >
+      <div class="load"  v-loading="isLoading"
+      element-loading-text="Loading..."
+      element-loading-svg-view-box="-10, -10, 50, 50"
+      element-loading-background="rgba(122, 122, 122, 0)"
+      style="width: 100%"> </div>
+      <!-- <el-loading lock text="正在加载..." spinner="el-icon-loading" background="rgba(255, 0, 0, 0.7)" v-model="isLoading" style="z-index:10"></el-loading> -->
       <template v-for="(message, index) in messages" :key="index">
         <!--时间-->
           <div class="system-message">{{message.time}}</div>
         <!--自己-->
-        <div v-if="message.role=='user'" class="chat-bubble left">
+        <div v-if="message.role=='user'" class="chat-bubble left" >
           <!-- 消息 -->
-          <span class="chat-bubble-right">
+          <span class="chat-bubble-right" >
      		  <div class="chat-text">{{message.content}}</div>
-            <img v-if="message.picture!=null" :src="message.picture" style="height: 200px;">
+            <img v-if="message.picture!=null" :src="message.picture" style="height: 200px;" @click="showDialog_user[index]=true">
+            
+            <!-- <el-button v-if="message.picture!=null && showButton[index]" class="button-below" type="info" plain>存草稿</el-button> -->
           </span>
           <span class="triangle-top-right"></span>
           <!-- 头像 -->
            <img :src="message.photo" class="user-avatar">
+          
         </div>
+        
         <!--对方-->
         <div v-else class="chat-bubble right">
           <!-- 头像 -->
            <img src="https://graphcrafter.oss-cn-beijing.aliyuncs.com/1/06012024191642.jpg" class="user-avatar">
           <!-- 消息 -->
-          <span class="triangle-top-left" :class="{ 'change-border': isLeftBubbleHover }"></span>
+          <span class="triangle-top-left" :class="{ 'change-border': isLeftBubbleHover }" ></span>
            <span class="chat-bubble-left">
 <!--       <span class="chat-bubble-left" @mouseover="changeLeftArrowColor(true)" @mouseout="changeLeftArrowColor(false)"> -->
              <div class="chat-text">{{message.content}}</div>
-             <img :src="message.picture" style="height: 200px;">
+             <img :src="message.picture" style="height: 200px;" @click="showDialog_gpt[index]=true">
+             <!-- <el-button v-if="showButton_gpt[index]" class="button-below-gpt" type="info" plain @click="saveAsDraft_user">存草稿</el-button> -->
+
      	     </span>
         </div>
 
+        <el-dialog  v-model="showDialog_user[index]" width="500" >
+        <el-image :src="message.picture"></el-image>        
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="showDialog_user[index] = false">取 消</el-button>
+          <el-button type="primary" @click="saveAsDraft_user(index)">存草稿</el-button>
+        </span>
+        </el-dialog>
+
+        <el-dialog  v-model="showDialog_gpt[index]" width="500" >
+        <el-image :src="message.picture"></el-image>        
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="showDialog_gpt[index] = false">取 消</el-button>
+          <el-button type="primary" @click="saveAsDraft_gpt(index)">存草稿</el-button>
+        </span>
+        </el-dialog>
       </template>
     </div>
+    
     <!--发送框-->
     <div class="chat-footer">
 
@@ -77,20 +106,32 @@ import store from "../store/index";
 import { ref,onMounted } from 'vue';
 import "https://gosspublic.alicdn.com/aliyun-oss-sdk-6.18.0.min.js";
 import { formatDateTime } from '@/utils/socket';
+import { ElMessage } from 'element-plus';
+import { fa } from 'element-plus/es/locale/index.mjs';
+import { ElLoading } from 'element-plus';
 
 export default {
   data(){
     return{
+      // isLoading:false,
       isLeftBubbleHover: false,
       selectMessageIndex:0,
       showMenu: true,
       menuX: -100,
       menuY: -100,
+      showButton: [],
+      showDialog_gpt: [],
       fileList:[],
+      showDialog_user: [],
     }
   },
   emits: ['close'],
   methods: {
+    // hideButton(index) {
+    //   setTimeout(() => {
+    //     this.showButton[index] = false;
+    //   }, 1000); // delay in milliseconds
+    // },
     uploadFile(params) {
       this.file = params.file;
       const client = new OSS({
@@ -121,6 +162,11 @@ export default {
           const result = await client.put(imgName, data, options);
           //存储图片的url至本地消息中
           const imgURL = "https://graphcrafter.oss-cn-beijing.aliyuncs.com/" + imgName;
+          axios.post('/api/upload_Picture',{
+            img:imgURL,
+            prompt:null,
+            Ptype:0
+          })
           console.log(imgURL);
           return imgURL;
         } catch (e) {
@@ -159,13 +205,43 @@ export default {
     },
     handleExceed(files, fileList) {
       this.$message.warning(`只允许上传 1 个文件`);
+    },
+    saveAsDraft_user(index){
+      this.showDialog_user[index]=false;
+      console.log('button clicked');
+      axios.post('/api/post_draft/',{
+        img:this.messages[index].picture,
+        user_id:store.state.user_id,
+        label:0
+      }).then(function (response) {
+        console.log(response.data['img']);
+      }).catch(function (error) {
+        console.log(error);
+      });
+      ElMessage.success('图片已保存至草稿箱！');
+    },
+    saveAsDraft_gpt(index){
+      this.showDialog_gpt[index]=false;
+      console.log('button clicked');
+      axios.post('/api/post_draft/',{
+        img:this.messages[index].picture,
+        user_id:store.state.user_id,
+        label:2
+      }).then(function (response) {
+        console.log(response.data['img']);
+      }).catch(function (error) {
+        console.log(error);
+      });
+      ElMessage.success('图片已保存至草稿箱！');
     }
+    
   },
   setup() {
     const text=ref("");
     let messages=ref([]);
     let img_url=ref("");
     let info=ref();
+    const isLoading = ref(false);
     axios.post("/api/get_info",{
       user_id:store.state.user_id
     }).then(function (response) {
@@ -186,7 +262,10 @@ export default {
         photo: info.value // 使用 info.value 而不是 info
       };
       messages.value.push(newMessage);
+      isLoading.value=true;
 
+      // const loading = ElLoading.service({ lock: false ,background: 'rgba(0, 0, 0, 0)',text: 'Loading',});
+      
       text.value='';
       axios.post('/api/chat_P2P',{
         img_url:img_url.value,
@@ -201,9 +280,15 @@ export default {
         };
         messages.value.push(newMsg);
         img_url.value=response.data['img'];
+        isLoading.value=false;
+        // loading.close();
         }).catch(function (error) {
           console.log(error);
+          // loading.close();
+          isLoading.value=false;
+
         });
+        
     }
 
 
@@ -216,7 +301,8 @@ export default {
       messages,
       sendMsg,
       img_url,
-      clearMsg
+      clearMsg,
+      isLoading
     };
   }
 };
@@ -288,8 +374,25 @@ export default {
   border-style: solid;
   margin-top: 5px;
 }
+.button-below {
+  position: absolute;
+  bottom: 0; /* Adjust this value as needed */
+  right:50%;
+  /* align-items: center; */
+  transform: translateX(50%);
+  z-index: 3;
+}
+
+.button-below-gpt {
+  position: absolute;
+  bottom: 0; /* Adjust this value as needed */
+  left:50%;
+  transform: translateX(-50%);
+  z-index: 3;
+}
 
 .chat-bubble {
+  /* position: relative; */
   margin: 20px 0;
   display: -webkit-flex;
   display: flex;
@@ -303,6 +406,7 @@ export default {
 }
 
 .chat-bubble .chat-bubble-left {
+  position: relative;
   padding: 10px;
   border-radius: 5px;
   background-color: #fdfdfe;
@@ -313,6 +417,8 @@ export default {
   background-color: #ebebeb;
 }
 .chat-bubble-right {
+  position: relative;
+
   padding: 10px;
   background-color: #80b9f2;
   border-radius: 5px;
@@ -394,6 +500,7 @@ export default {
 }
 
 .message-container {
+  position:relative;
   border-top: 1px solid #e9eaec;
   border-bottom: 1px solid #e9eaec;
   height: 600px;
@@ -402,6 +509,12 @@ export default {
   /*border-radius: 8px;*/
   padding: 8px;
 
+}
+.load{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .system-message {
